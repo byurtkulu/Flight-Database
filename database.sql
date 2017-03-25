cc85a3f7577011eaeba5a642d399f9a4692aee5a
Oran Can Ören <orancanoren@gmail.com>
2017-03-25 13:47:58 +0200

    Update database.sql

--- database.sql
+++ database.sql
@@ -1,3 +1,4 @@
+DROP DATABASE IF EXISTS flight_cs306;
 CREATE DATABASE IF NOT EXISTS flight_cs306;
 USE flight_cs306;
 
@@ -6,30 +7,37 @@ USE flight_cs306;
 --
 
 CREATE TABLE IF NOT EXISTS `flights` (
-	`flight_ID`CHAR(4),
+	`flight_number`CHAR(7),
 	`plane_model` CHAR(20),
+	`tail_number` CHAR(6),
+	`gate/park` CHAR(5),
 	`departure_time` TIMESTAMP,
 	`isDomestic` BOOLEAN,
-	PRIMARY KEY (`flight_ID`)
+	`destination` CHAR(3),
+	`departure` CHAR(3),
+	`runway` CHAR(5),
+	PRIMARY KEY (`flight_number`)
 );
 
-CREATE TABLE IF NOT EXISTS `airway` (
-	`airway_ID` CHAR(4), -- IATA states that airway ID's consist of 4 chars
-	`airway_name` CHAR(20),
-	PRIMARY KEY (`airway_ID`)
+CREATE TABLE IF NOT EXISTS `airlines` (
+	`airline_code` CHAR(4), -- IATA states that airlines ID's consist of 4 chars
+	`airline_name` CHAR(20),
+	PRIMARY KEY (`airline_code`)
 );
 
-CREATE TABLE IF NOT EXISTS `employee` (
+CREATE TABLE IF NOT EXISTS `employees` (
 	`ssn` INTEGER,
 	`name` CHAR(20),
 	`service_type` CHAR(20),
 	PRIMARY KEY (`ssn`)
 );
 
-CREATE TABLE IF NOT EXISTS `passenger` (
+CREATE TABLE IF NOT EXISTS `passengers` (
 	`class` CHAR(11), -- since max length is 11 with "first class"
 	`name` CHAR(20),
-	`customer_no` INTEGER,
+	`customer_no` VARCHAR(11),
+	`age` INTEGER,
+	`isFemale` BOOLEAN,
 	PRIMARY KEY (`customer_no`)
 );
 
@@ -38,29 +46,29 @@ CREATE TABLE IF NOT EXISTS `passenger` (
 --
 
 CREATE TABLE IF NOT EXISTS `belongs` (
-	`airway_ID` CHAR(4),
-	`flight_ID` CHAR(4),
-	FOREIGN KEY (`airway_ID`) REFERENCES `airway`(`airway_ID`),
-	FOREIGN KEY (`flight_ID`) REFERENCES `flights`(`flight_ID`),
-	PRIMARY KEY (`airway_ID`, `flight_ID`)
+	`airline_code` CHAR(4),
+	`flight_number` CHAR(7),
+	FOREIGN KEY (`airline_code`) REFERENCES `airlines`(`airline_code`),
+	FOREIGN KEY (`flight_number`) REFERENCES `flights`(`flight_number`),
+	PRIMARY KEY (`airline_code`, `flight_number`)
 );
 
 CREATE TABLE IF NOT EXISTS `works_in` (
-	`since` DATETIME,
-	`airway_ID` CHAR(4),
+	`since` DATE,
+	`airline_code` CHAR(4),
 	`ssn` INTEGER,
-	FOREIGN KEY (`airway_ID`) REFERENCES `airway`(`airway_ID`),
-	FOREIGN KEY (`ssn`) REFERENCES `employee`(`ssn`),
-	PRIMARY KEY (`ssn`, `airway_ID`)
+	FOREIGN KEY (`airline_code`) REFERENCES `airlines`(`airline_code`),
+	FOREIGN KEY (`ssn`) REFERENCES `employees`(`ssn`),
+	PRIMARY KEY (`ssn`, `airline_code`)
 );
 
 CREATE TABLE IF NOT EXISTS `flies` (
 	`cost` INTEGER,
-	`customer_no` INTEGER,
-	`flight_ID` CHAR(4),
-	FOREIGN KEY (`flight_ID`) REFERENCES flights(`flight_ID`),
-	FOREIGN KEY (`customer_no`) REFERENCES `passenger`(`customer_no`),
-	PRIMARY KEY (`flight_ID`, `customer_no`)
+	`customer_no` VARCHAR(11),
+	`flight_number` CHAR(7),
+	FOREIGN KEY (`flight_number`) REFERENCES flights(`flight_number`),
+	FOREIGN KEY (`customer_no`) REFERENCES `passengers`(`customer_no`),
+	PRIMARY KEY (`flight_number`, `customer_no`)
 );
 
 --
@@ -68,9 +76,9 @@ CREATE TABLE IF NOT EXISTS `flies` (
 --
 
 CREATE TABLE IF NOT EXISTS `contracted` (
-	`contract_ID` INTEGER,  /*INTEGER???*/
+	`contract_ID` INTEGER, 
 	`employee_ssn` INTEGER,
-	FOREIGN KEY (`employee_ssn`) REFERENCES `employee`(`ssn`),
+	FOREIGN KEY (`employee_ssn`) REFERENCES `employees`(`ssn`),
 	PRIMARY KEY (`employee_ssn`)
 );
 
@@ -78,20 +86,19 @@ CREATE TABLE IF NOT EXISTS `hourly` (
 	`hourly_wages` INTEGER,
 	`hours_worked` INTEGER,
 	`employee_ssn` INTEGER,
-	FOREIGN KEY (`employee_ssn`) REFERENCES `employee`(`ssn`),
+	FOREIGN KEY (`employee_ssn`) REFERENCES `employees`(`ssn`),
 	PRIMARY KEY (`employee_ssn`)
 );
 
 CREATE TABLE IF NOT EXISTS `passaport_entry` (
-	`passaport_ID` INTEGER,
-	`customer_no` INTEGER,
-	FOREIGN KEY (`customer_no`) REFERENCES `passenger`(`customer_no`),
-	PRIMARY KEY (`customer_no`)
+	`passaport_ID` VARCHAR(11),
+	`country_code` CHAR(2), -- eg. TR, EN...
+	FOREIGN KEY (`passaport_ID`) REFERENCES `passengers`(`customer_no`),
+	PRIMARY KEY (`passaport_ID`)
 );
 
 CREATE TABLE IF NOT EXISTS `turkish_entry` (
-	`turkish_ID` INTEGER,
-	`customer_no` INTEGER,
-	FOREIGN KEY (`customer_no`) REFERENCES `passenger`(`customer_no`),
-	PRIMARY KEY (`customer_no`)
+	`turkish_ID` VARCHAR(11),
+	FOREIGN KEY (`turkish_ID`) REFERENCES `passengers`(`customer_no`),
+	PRIMARY KEY (`turkish_ID`)
 );
